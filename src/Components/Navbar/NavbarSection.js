c5c3eb7675d43b3c4547ac8f129ebbc9f6ecd03a
souraavsen1 <sourav15-11392@diu.edu.bd>
2021-10-19 00:21:53 +0600

    feat:navbar updated.

--- src/Components/Navbar/NavbarSection.js
+++ src/Components/Navbar/NavbarSection.js
@@ -9,34 +9,39 @@ const NavbarSection = () => {
         <Container className='mx-20'>
           <div className='flex justify-center items-center'>
             <img src={logo} className='mr-2' width='25px' alt='' />
-            <Navbar.Brand className='font-semibold text-2xl' href='/'>Be Better</Navbar.Brand>
+            <Navbar.Brand className='font-semibold text-2xl' href='/'>
+              Be Better
+            </Navbar.Brand>
           </div>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link className='mr-2' href='/'>
+              <Nav.Link
+                className='mr-2 text-black border-b-2 border-transparent hover:border-red-400'
+                href='/'
+              >
                 Home
               </Nav.Link>
-              <Nav.Link className='mr-2' href='/about'>
+              <Nav.Link className='mr-2 text-black border-b-2 border-transparent hover:border-red-400' href='/about'>
                 About
               </Nav.Link>
-              <Nav.Link className='mr-2' href='#link'>
+              <Nav.Link className='mr-2 text-black border-b-2 border-transparent hover:border-red-400' href='#link'>
                 Services
               </Nav.Link>
               <Nav.Link
-                className='px-3 py-1 mr-2 my-auto hover:bg-red-300 hover:text-white font-semibold rounded-full border-2 border-red-300'
+                className='px-3 py-1 mr-2 my-auto hover:bg-red-300 hover:text-white rounded-full border-2 border-red-400 text-black'
                 href='/signup'
               >
                 Sign up
               </Nav.Link>
               <Nav.Link
-                className='px-3 py-1 mr-2 my-auto hover:bg-red-300 hover:text-white font-semibold rounded-full border-2 border-red-300'
+                className='px-3 py-1 mr-2 my-auto hover:bg-red-300 hover:text-white rounded-full border-2 border-red-400 text-black'
                 href='/signin'
               >
                 Sign in
               </Nav.Link>
               <Nav.Link
-                className='px-3 py-1 mr-2 my-auto hover:bg-red-300 hover:text-white font-semibold rounded-full border-2 border-red-300'
+                className='px-3 py-1 mr-2 my-auto hover:bg-red-300 hover:text-white rounded-full border-2 border-red-400 text-black'
                 href='#signin'
               >
                 Log out
